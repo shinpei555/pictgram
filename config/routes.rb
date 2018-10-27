6fee81f1a4bfdcba5d6e9440ca9e7adfba2724b2
阿部　慎平 <shinpei@StartToday.local>
2018-10-27 09:22:41 +0900

    いいね機能まで実装すみ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get 'users/new'
+
+
+  get 'sessions/new'
+
   root 'pages#index'
   get 'pages/help'
+
+  resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :topics
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
 end
